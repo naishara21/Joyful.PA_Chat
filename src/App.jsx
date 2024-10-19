b850430e1d0835d4cc00ef5c6332c49a61e6959a
Naisha <86354320+naishara21@users.noreply.github.com>
2024-10-20 01:56:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   async function generateAnswer(e) {
     setGeneratingAnswer(true);
     e.preventDefault();
-    setAnswer("Loading your answer... \n It might take up to 10 seconds");
+    setAnswer("Loading your answer... \n It might take upto 10 seconds");
     try {
       const response = await axios({
         url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${
@@ -35,35 +35,37 @@ function App() {
   }
 
   return (
-    <div className="bg-gradient-to-r from-blue-50 to-blue-100 min-h-screen p-4 flex flex-col justify-center items-center">
-      <form
-        onSubmit={generateAnswer}
-        className="w-full max-w-lg text-center rounded-lg shadow-xl bg-white py-6 px-4 transition-transform duration-500 transform hover:scale-105"
+    <>
+    <div className="bg-gradient-to-r from-blue-50 to-blue-100 h-screen p-3 flex flex-col justify-center items-center">
+    <form
+      onSubmit={generateAnswer}
+      className="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 text-center rounded-lg shadow-lg bg-white py-6 px-4 transition-all duration-500 transform hover:scale-105"
+    >
+      <a href="https://github.com/Vishesh-Pandey/chat-ai" target="_blank" rel="noopener noreferrer">
+        <h1 className="text-4xl font-bold text-blue-500 mb-4 animate-bounce">Chat AI</h1>
+      </a>
+      <textarea
+        required
+        className="border border-gray-300 rounded w-full my-2 min-h-fit p-3 transition-all duration-300 focus:border-blue-400 focus:shadow-lg"
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
+        placeholder="Ask anything"
+      ></textarea>
+      <button
+        type="submit"
+        className={`bg-blue-500 text-white p-3 rounded-md hover:bg-blue-600 transition-all duration-300 ${
+          generatingAnswer ? 'opacity-50 cursor-not-allowed' : ''
+        }`}
+        disabled={generatingAnswer}
       >
-        <a href="https://github.com/Vishesh-Pandey/chat-ai" target="_blank" rel="noopener noreferrer">
-          <h1 className="text-5xl font-bold text-[#0B3040] mb-6 animate-bounce">Chat AI</h1>
-        </a>
-        <textarea
-          required
-          className="border border-gray-300 rounded w-full my-2 min-h-fit p-4 transition-all duration-300 focus:border-[#0B3040] focus:shadow-lg"
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-          placeholder="Ask anything"
-        ></textarea>
-        <button
-          type="submit"
-          className={`bg-[#0B3040] text-white p-4 rounded-md hover:bg-blue-600 transition-all duration-300 ${
-            generatingAnswer ? 'opacity-50 cursor-not-allowed' : ''
-          }`}
-          disabled={generatingAnswer}
-        >
-          Generate answer
-        </button>
-      </form>
-      <div className="w-full max-w-lg text-center rounded-lg bg-white my-6 shadow-xl transition-transform duration-500 transform hover:scale-105">
-        <ReactMarkdown className="p-6 text-[#0B3040]">{answer}</ReactMarkdown>
-      </div>
+        Generate answer
+      </button>
+    </form>
+    <div className="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 text-center rounded-lg bg-white my-4 shadow-lg transition-all duration-500 transform hover:scale-105">
+      <ReactMarkdown className="p-4">{answer}</ReactMarkdown>
     </div>
+  </div>
+    </>
   );
 }
 
